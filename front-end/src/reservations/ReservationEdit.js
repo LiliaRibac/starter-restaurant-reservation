51400f72932d765ee5111d7ebb8ce5fb160e58fd
lilia <ribac.lilia@gmail.com>
2024-06-21 11:50:47 -0500

    fix the editComponent and is working Now

--- front-end/src/reservations/ReservationEdit.js
+++ front-end/src/reservations/ReservationEdit.js
@@ -2,9 +2,8 @@ import React, { useState, useEffect } from 'react';
 import { useHistory, useParams } from 'react-router-dom';
 import ErrorAlert from '../layout/ErrorAlert';
 import { readReservation, updateReservation } from '../utils/api';
-import formatReservationDate from '../utils/format-reservation-date';
-import formatReservationTime from '../utils/format-reservation-time';
-
+import { formatAsDate, formatAsTime } from '../utils/date-time';
+import ReservationForm from './ReservationForm';
 // Initial form state
 const initialFormState = {
   first_name: '',
@@ -37,12 +36,13 @@ const EditReservation = () => {
           reservation_id,
           abortController.signal
         );
+        console.log(data);
 
         // Format the fetched data
         const formattedReservation = {
           ...data,
-          reservation_date: formatReservationDate(data.reservation_date),
-          reservation_time: formatReservationTime(data.reservation_time),
+          reservation_date: formatAsDate(data.reservation_date),
+          reservation_time: formatAsTime(data.reservation_time),
         };
 
         setFormData(formattedReservation);
@@ -63,11 +63,12 @@ const EditReservation = () => {
     return () => abortController.abort();
   }, [reservation_id]);
 
-  // Handle input change
   const handleChange = ({ target }) => {
+    const value =
+      target.name === 'people' ? Number(target.value) : target.value;
     setFormData({
       ...formData,
-      [target.name]: target.value,
+      [target.name]: value,
     });
   };
 
@@ -86,100 +87,15 @@ const EditReservation = () => {
   if (isLoading) return <p>Loading...</p>;
 
   return (
-    <form onSubmit={handleSubmit} className='form-group'>
-      <div className='row'>
-        <label htmlFor='first_name' className='col'>
-          First name:
-          <input
-            type='text'
-            id='first_name'
-            name='first_name'
-            onChange={handleChange}
-            value={formData.first_name}
-            className='form-control'
-            required
-          />
-        </label>
-        <label htmlFor='last_name' className='col'>
-          Last name:
-          <input
-            type='text'
-            id='last_name'
-            name='last_name'
-            onChange={handleChange}
-            value={formData.last_name}
-            className='form-control'
-            required
-          />
-        </label>
-      </div>
-      <div className='row'>
-        <label htmlFor='reservation_date' className='col'>
-          Date:
-          <input
-            type='date'
-            id='reservation_date'
-            name='reservation_date'
-            onChange={handleChange}
-            value={formData.reservation_date}
-            className='form-control'
-            required
-          />
-        </label>
-        <label htmlFor='reservation_time' className='col'>
-          Time:
-          <input
-            type='time'
-            id='reservation_time'
-            name='reservation_time'
-            onChange={handleChange}
-            value={formData.reservation_time}
-            className='form-control'
-            required
-          />
-        </label>
-      </div>
-      <div className='row'>
-        <label htmlFor='people' className='col'>
-          Number of people:
-          <input
-            type='number'
-            id='people'
-            name='people'
-            min='1'
-            onChange={handleChange}
-            value={formData.people}
-            className='form-control'
-            required
-          />
-        </label>
-        <label htmlFor='mobile_number' className='col'>
-          Mobile number:
-          <input
-            type='text'
-            id='mobile_number'
-            name='mobile_number'
-            onChange={handleChange}
-            value={formData.mobile_number}
-            className='form-control'
-            required
-          />
-        </label>
-      </div>
-      <button type='submit' className='btn btn-primary'>
-        Submit
-      </button>
-      <button
-        type='button'
-        onClick={() => history.goBack()}
-        className='btn btn-danger'
-      >
-        Cancel
-      </button>
-      <div className='row'>
-        <ErrorAlert error={reservationError} />
-      </div>
-    </form>
+    <main>
+      <h1>Edit Reservation</h1>
+      <ErrorAlert error={reservationError} />
+      <ReservationForm
+        reservation={formData}
+        handleChange={handleChange}
+        handleSubmit={handleSubmit}
+      />
+    </main>
   );
 };
 
